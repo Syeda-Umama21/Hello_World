cc2a196d2dfb56ed430ac22fe936cc296a05cd8e
Umama-Shah <165512076+Umama-Shah@users.noreply.github.com>
2024-04-23 15:33:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+//This will print the text Hello world! to the console.
+console.log("Hello World!");
---
+++ main.ts
@@ -0,0 +1,2 @@
+//This code will print the text Hello world! to the console.
+console.log("Hello World!");
